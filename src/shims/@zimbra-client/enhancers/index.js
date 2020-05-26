6f1ee048e62a42fb9ae82cff9e9bb3308eec6a74
BhavinBathani <bhavin.bathani@synacor.com>
2020-05-26 21:26:54 +0530

    Revert "PREAPPS-4958 : Adding changes related to rename registerTab to withTabManager."
    This reverts commit 0ed86d624bbf182fc1efe3b8f71beedfd9750a27.

--- src/shims/@zimbra-client/enhancers/index.js
+++ src/shims/@zimbra-client/enhancers/index.js
@@ -1,18 +1,14 @@
 /** This file is an auto-generated shim, aliased in for "@zimbra-client/enhancers" in the webpack config.
- *  When components import '@zimbra-client/enhancers', we want to give them back the copy
- *  Zimbra passed down when it called the factory provided to zimlet().
- */
+*  When components import '@zimbra-client/enhancers', we want to give them back the copy
+*  Zimbra passed down when it called the factory provided to zimlet().
+*/
 
 /* eslint-disable camelcase, dot-notation */
-import { warnOnMissingExport } from "../../";
-const wrap = warnOnMissingExport.bind(
-	null,
-	global.shims["@zimbra-client/enhancers"],
-	"@zimbra-client/enhancers"
-);
+import { warnOnMissingExport } from '../../';
+const wrap = warnOnMissingExport.bind(null, global.shims['@zimbra-client/enhancers'], '@zimbra-client/enhancers');
 
-export const withMediaQuery = wrap("withMediaQuery");
-export const withTabManager = wrap("withTabManager");
-export const withIntlWrapper = wrap("withIntlWrapper");
+export const withMediaQuery = wrap('withMediaQuery');
+export const registerTab = wrap('registerTab');
+export const withIntlWrapper = wrap('withIntlWrapper');
 
-export default global.shims["@zimbra-client/enhancers"];
+export default global.shims['@zimbra-client/enhancers'];
