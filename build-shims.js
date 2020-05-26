0ed86d624bbf182fc1efe3b8f71beedfd9750a27
BhavinBathani <bhavin.bathani@synacor.com>
2020-05-26 21:20:14 +0530

    PREAPPS-4958 : Adding changes related to rename registerTab to withTabManager.

--- build-shims.js
+++ build-shims.js
@@ -1,58 +1,58 @@
-const fs = require('fs');
-const path = require('path');
-const mkdirp = require('mkdirp');
-const mockery = require('mockery');
-const shimmedModules = require('./src/shims/index.js').SHIMMED_MODULES;
+const fs = require("fs");
+const path = require("path");
+const mkdirp = require("mkdirp");
+const mockery = require("mockery");
+const shimmedModules = require("./src/shims/index.js").SHIMMED_MODULES;
 
 mockery.enable({
 	warnOnReplace: false,
-	warnOnUnregistered: false
+	warnOnUnregistered: false,
 });
 
-mockery.registerSubstitute('react', 'preact/compat');
-mockery.registerSubstitute('react-dom', 'preact/compat');
-mockery.registerSubstitute('react-dom/server', 'preact/compat');
-mockery.registerMock('preact-router/match', { Match: 1 });
-mockery.registerMock('apollo-client', {}); //doesn't really matter
-mockery.registerMock('redux', {}); //doesn't really matter
+mockery.registerSubstitute("react", "preact/compat");
+mockery.registerSubstitute("react-dom", "preact/compat");
+mockery.registerSubstitute("react-dom/server", "preact/compat");
+mockery.registerMock("preact-router/match", { Match: 1 });
+mockery.registerMock("apollo-client", {}); //doesn't really matter
+mockery.registerMock("redux", {}); //doesn't really matter
 
 /* @zimbra/util is a psuedo-module: it is built from the many library
  * functions contained in `zm-x-web`.
  */
-mockery.registerMock('@zimbra-client/util', {
+mockery.registerMock("@zimbra-client/util", {
 	array: 1,
 	getDataTransferJSON: 1,
 	setDataTransferJSON: 1,
 	breakpoints: 1,
 	callWith: 1,
-	pruneEmpty: 1
+	pruneEmpty: 1,
 });
 
-mockery.registerMock('@zimbra-client/browser', {
-	openExternalBrowser: 1
+mockery.registerMock("@zimbra-client/browser", {
+	openExternalBrowser: 1,
 });
 
-mockery.registerMock('@zimbra-client/util/contacts', {
-	getName: 1
+mockery.registerMock("@zimbra-client/util/contacts", {
+	getName: 1,
 });
 
-mockery.registerMock('@zimbra-client/util/redux', {
+mockery.registerMock("@zimbra-client/util/redux", {
 	paginate: 1,
 	createAsyncAction: 1,
 	pendingAction: 1,
 	fulfilledAction: 1,
-	rejectedAction: 1
+	rejectedAction: 1,
 });
 
-mockery.registerMock('graphql-tag', {
-	default: 1
+mockery.registerMock("graphql-tag", {
+	default: 1,
 });
 
-mockery.registerMock('preact-pwa-install', {
-	default: 1
+mockery.registerMock("preact-pwa-install", {
+	default: 1,
 });
 
-mockery.registerMock('@zimbra-client/graphql', {
+mockery.registerMock("@zimbra-client/graphql", {
 	AppointmentsQuery: 1,
 	CalendarsAndAppointmentsQuery: 1,
 	withCreateAppointment: 1,
@@ -66,20 +66,20 @@ mockery.registerMock('@zimbra-client/graphql', {
 	CalendarCreateMutation: 1,
 	withCreateCalendar: 1,
 	withSetCustomMetaData: 1,
-	withGetCustomMetaData: 1
+	withGetCustomMetaData: 1,
 });
 
-mockery.registerMock('@zimbra-client/enhancers', {
+mockery.registerMock("@zimbra-client/enhancers", {
 	withMediaQuery: 1,
-	registerTab: 1,
-	withIntlWrapper: 1
+	withTabManager: 1,
+	withIntlWrapper: 1,
 });
 
-mockery.registerMock('@zimbra-client/constants', {
+mockery.registerMock("@zimbra-client/constants", {
 	ATTENDEE_ROLE: 1,
-	PARTICIPATION_STATUS: 1
+	PARTICIPATION_STATUS: 1,
 });
-mockery.registerMock('@zimbra-client/blocks', {
+mockery.registerMock("@zimbra-client/blocks", {
 	Dialog: 1,
 	FixedDialog: 1,
 	ModalDialog: 1,
@@ -100,10 +100,10 @@ mockery.registerMock('@zimbra-client/blocks', {
 	AffixBottom: 1,
 	KeyCodes: 1,
 	Label: 1,
-	ChoiceInput: 1
+	ChoiceInput: 1,
 });
 
-mockery.registerMock('@zimbra-client/components', {
+mockery.registerMock("@zimbra-client/components", {
 	MenuItem: 1,
 	Sidebar: 1,
 	MailSidebar: 1,
@@ -142,17 +142,19 @@ mockery.registerMock('@zimbra-client/components', {
 	AttachmentItem: 1,
 	ResponsiveModal: 1,
 	NestedActionMenuItem: 1,
-	PdfjsViewer: 1
+	PdfjsViewer: 1,
 });
 
-mockery.registerMock('@zimbra-client/errors', {
+mockery.registerMock("@zimbra-client/errors", {
 	errorMessage: 1,
-	faultCode: 1
+	faultCode: 1,
 });
 
 function createShim(shimModule) {
 	if (Array.isArray(shimModule)) {
-		shimModule.map((name, index) => !index ? name : `${shimModule[0]}/${name}`).forEach(createShim);
+		shimModule
+			.map((name, index) => (!index ? name : `${shimModule[0]}/${name}`))
+			.forEach(createShim);
 		return;
 	}
 	// console.log('require.cache', require.cache);
@@ -160,23 +162,40 @@ function createShim(shimModule) {
 	let dirName = path.resolve(`src/shims/${shimModule}`);
 
 	mkdirp(dirName, (err) => {
-		if (err) { console.error(`Unable to mkdir ${dirName}`); return; }
+		if (err) {
+			console.error(`Unable to mkdir ${dirName}`);
+			return;
+		}
 
-		fs.writeFileSync(`${dirName}/index.js`,
+		fs.writeFileSync(
+			`${dirName}/index.js`,
 			`/** This file is an auto-generated shim, aliased in for "${shimModule}" in the webpack config.
 *  When components import '${shimModule}', we want to give them back the copy
 *  Zimbra passed down when it called the factory provided to zimlet().
 */
 
 /* eslint-disable camelcase, dot-notation */
-import { warnOnMissingExport } from '.${shimModule.split('/').map((pathpart, index) => !index ? './' : '../').join('')}';
+import { warnOnMissingExport } from '.${shimModule
+				.split("/")
+				.map((pathpart, index) => (!index ? "./" : "../"))
+				.join("")}';
 const wrap = warnOnMissingExport.bind(null, global.shims['${shimModule}'], '${shimModule}');
 
-${Object.keys(require(shimModule)).filter(exportName => exportName !== '__esModule').map(exportName =>
-		`export ${exportName === 'default' ? 'default' : `const ${exportName} =`} wrap('${exportName}');`).join('\n')
+${Object.keys(require(shimModule))
+	.filter((exportName) => exportName !== "__esModule")
+	.map(
+		(exportName) =>
+			`export ${
+				exportName === "default" ? "default" : `const ${exportName} =`
+			} wrap('${exportName}');`
+	)
+	.join("\n")}
+${
+	"default" in require(shimModule)
+		? ""
+		: `
+export default global.shims['${shimModule}'];`
 }
-${'default' in require(shimModule) ? '' : `
-export default global.shims['${shimModule}'];`}
 `
 		);
 	});
@@ -184,4 +203,4 @@ export default global.shims['${shimModule}'];`}
 
 shimmedModules.forEach(createShim);
 
-console.log('Successfully Built Shim files');
+console.log("Successfully Built Shim files");
