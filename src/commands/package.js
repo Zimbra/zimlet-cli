d4a018a6cb3060a17063955a22b70e9b62a51ded
MiteshZimbra <mitesh.savani@synacor.com>
2022-04-19 16:27:05 +0530

    PREAPPS-6344: Add property file with label and description value for every language available in src/intl folder in the package - backward compatibility support

--- src/commands/package.js
+++ src/commands/package.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import Zip from 'adm-zip';
-import { error } from '../util';
+import { error, warn } from '../util';
 import asyncCommand from '../lib/async-command';
 
 export default asyncCommand({
@@ -26,13 +26,12 @@ export default asyncCommand({
 		description: {
 			alias: 'desc',
 			description: 'Description for your zimlet',
-			demandOption: true,
-			requiresArg: true
+			default: null
 		},
 		label: {
 			alias: 'l',
 			description: 'Display name for your zimlet',
-			demandOption: true
+			default: null
 		},
 		zimbraXVersion: {
 			description: 'https://semver.org version range of Zimbra X that the Zimlet is compatible with.',
@@ -59,10 +58,17 @@ export default asyncCommand({
 		let builddir = path.resolve(cwd, argv.builddir || 'build');
 		let dest = path.resolve(cwd, argv.dest || 'pkg', `${argv.name}.zip`);
 
+		// Get label and description from src/intl/en_US.json or from argument
+		const { label, description } = getZimletLabelDescription() || argv;
+
+		if (!(label && description)) {
+			return error('label and description string are not provided as argument');
+		}
+
 		// Create the xml descriptor file for the zimlet
 		let xmlFile = `${argv.name}.xml`;
 
-		let zimletXML = `<zimlet name="${argv.name}" version="${argv.pkgVersion}" description="${argv.description}" label="${argv.label}" zimbraXZimletCompatibleSemVer="${argv.zimbraXVersion}">`;
+		let zimletXML = `<zimlet name="${argv.name}" version="${argv.pkgVersion}" description="${description}" label="${label}" zimbraXZimletCompatibleSemVer="${argv.zimbraXVersion}">`;
 
 		let files;
 		try {
@@ -72,17 +78,19 @@ export default asyncCommand({
 			return error(`Failed to read ${builddir}: ${err}`, 1);
 		}
 
-		files.forEach(file => {
-			if (file.match(/\.js$/)) {
-				zimletXML += `\n\t<include>${file}</include>`;
-			}
-			else if (file.match(/\.css$/)) {
-				zimletXML += `\n\t<includeCSS>${file}</includeCSS>`;
-			}
-			else if (file !== xmlFile) {
-				zimletXML += `\n\t<resource>${file}</resource>`;
-			}
-		});
+		files
+			.filter(file => !file.match(/\.properties$/))
+			.forEach(file => {
+				if (file.match(/\.js$/)) {
+					zimletXML += `\n\t<include>${file}</include>`;
+				}
+				else if (file.match(/\.css$/)) {
+					zimletXML += `\n\t<includeCSS>${file}</includeCSS>`;
+				}
+				else if (file !== xmlFile) {
+					zimletXML += `\n\t<resource>${file}</resource>`;
+				}
+			});
 
 		zimletXML += '\n</zimlet>';
 
@@ -93,19 +101,7 @@ export default asyncCommand({
 			return error(`Failed to write XML file: ${err}`, 1);
 		}
 
-		// Add properties file for description and label
-		// As admin console requires properties file to display label and desciption
-		let propertiesFile = `${argv.name}.properties`;
-		let zimletProperties = '\n';
-		zimletProperties += `label = ${argv.label}\n`;
-		zimletProperties += `description = ${argv.description}\n`;
-
-		try {
-			fs.writeFileSync(path.resolve(builddir, propertiesFile), zimletProperties);
-		}
-		catch (err) {
-			return error(`Failed to write properties file: ${err}`, 1);
-		}
+		createLocalizationFiles(argv.name, label, description);
 
 		//Zip up the contents of the build dir along with the xml file as the final zimlet deliverable
 		let zipFile = new Zip();
@@ -116,3 +112,94 @@ export default asyncCommand({
 	}
 });
 
+
+function getZimletLabelDescription() {
+	const intlDir = path.resolve(process.cwd(), 'src', 'intl');
+
+	try {
+		const content = JSON.parse(
+			fs.readFileSync(path.resolve(intlDir, 'en_US.json'))
+		);
+
+		if (!(content.zimlet?.label && content.zimlet?.description)) {
+			throw new Error('label or description not found');
+		}
+		return {
+			label: content.zimlet.label,
+			description: content.zimlet.description
+		};
+	}
+	catch (err) {
+		warn(`Failed to read src/intl/en_US.json file: ${err}. \nMake sure below content present in src/intl/en_US.json file \n
+		"zimlet": {
+			"label": "<zimlet label>",
+			"description": "<zimlet description>"
+		} \nAs a fallback we are using strings from package.json which is deprecated.\n`);
+		return null;
+	}
+
+}
+
+// Add properties file for description and label
+// As admin console requires properties file to display label and desciption
+function createLocalizationFiles(zimletName, label, description) {
+	const cwd = process.cwd();
+	const intlDir = path.resolve(cwd, 'src', 'intl');
+	let intlFiles;
+
+	createPropertyFile(zimletName, label, description);
+
+	try {
+		intlFiles = fs.readdirSync(intlDir);
+	}
+	catch (err) {
+		console.error(`Failed to read ${intlDir}: ${err}`, 1);
+	}
+
+	intlFiles
+		.filter(file => file.match(/\.json$/) && !file.includes('en_US'))
+		.forEach(intl => {
+			try {
+				const content = JSON.parse(
+					fs.readFileSync(path.resolve(intlDir, intl))
+				);
+				const propertiesFileName = `${zimletName}_${intl.replace(
+					'.json',
+					''
+				)}`;
+				const labelValue = getUnicode(content.zimlet?.label || '');
+				const descriptionValue = getUnicode(content.zimlet?.description || '');
+
+				createPropertyFile(propertiesFileName, labelValue, descriptionValue);
+			}
+			catch (ex) {
+				error(`Error while reading file: ${ex}`, 1);
+			}
+		});
+}
+
+function createPropertyFile(fileName, label, description) {
+	const builddir = path.resolve(process.cwd(), 'build');
+	const propertiesFile = `${fileName}.properties`;
+	let zimletProperties = '\n';
+	zimletProperties += `label = ${label}\n`;
+	zimletProperties += `description = ${description}\n`;
+
+	try {
+		fs.writeFileSync(path.resolve(builddir, propertiesFile), zimletProperties);
+	}
+	catch (err) {
+		return error(`Failed to write properties file: ${err}`, 1);
+	}
+}
+
+function getUnicode(txt) {
+	let hex = '';
+	if (txt.length === 0) return;
+	for (let i = 0; i < txt.length; i++) {
+		const h = txt.codePointAt(i).toString(16);
+		hex += '\\u' + h.padStart(4, 0);
+		if (h.length > 4) i++;
+	}
+	return hex;
+}
