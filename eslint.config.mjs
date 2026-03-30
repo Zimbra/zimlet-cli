085b17e81939577b673173bef4c346d4d9e8297e
Saket Patel <saket.patel@synacor.com>
2026-03-30 17:44:35 +0530

    PREAPPS-9055 Update to latest syntax

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,31 @@
 import { coreJsConfig, customConfig } from '@zimbra/eslint-config';
 
-const finalConfig = [
-	...customConfig,
-	...coreJsConfig.map(conf => {
-		return {
-			...conf,
-			ignores: [...conf.ignores, 'src/shims/**'],
-			rules: {
-				...conf.rules,
-				'no-console': 'off'
-			}
-		};
-	})
+// Ignore directory patterns
+const globalIgnores = ['**/node_modules/**', 'build/**', 'dist/**', 'package-lock.json', 'package.json'];
+
+// Helper function to merge a specific key from multiple configs
+const mergeConfigKey = (configs, key) =>
+	Object.assign({}, ...configs.map(config => config[key] || {}));
+
+// Configs to merge for JavaScript files
+const jsConfigs = [coreJsConfig, customConfig];
+
+// Build JavaScript config dynamically
+const finalConfig = {
+	files: ['**/*.{js,mjs}'],
+	ignores: ['src/shims/**'],
+
+	languageOptions: coreJsConfig.languageOptions,
+
+	plugins: mergeConfigKey(jsConfigs, 'plugins'),
+	rules: mergeConfigKey(jsConfigs, 'rules'),
+	settings: mergeConfigKey(jsConfigs, 'settings')
+};
+
+export default [
+	{
+		ignores: globalIgnores
+	},
+	finalConfig
 ];
 
-export default finalConfig;
